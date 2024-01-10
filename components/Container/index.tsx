42398b3397373600581dd7e6899606210c2dfa81
abrxao <abrxao@gmail.com>
2024-01-10 18:03:09 -0300

    feat: new container component

---
+++ components/Container/index.tsx
@@ -0,0 +1,25 @@
+import { FunctionComponent, ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
+
+interface ContainerProps {
+  children: ReactNode;
+  className?: string;
+}
+
+const Container: FunctionComponent<ContainerProps> = ({
+  children,
+  className,
+}) => {
+  return (
+    <div
+      className={twMerge(
+        "px-3 xs:px-4 py-3 lg:py-5 w-full lg:px-12 xl:px-16 2xl:px-24",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Container;
