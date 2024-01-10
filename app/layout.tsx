17b5107d0cf9ce076effde82465b36e21fd7e4e9
abrxao <abrxao@gmail.com>
2024-01-10 18:02:38 -0300

    :feat: first http request to get sample notes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,19 @@
+"use client";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import ModeToggle from "@/components/ModeToggleButton";
+import { QueryClientProvider } from "react-query";
+import { queryClient } from "@/lib/queryClient";
+import Container from "@/components/Container";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
+/* export const metadata: Metadata = {
   title: "Take a note",
   description: "Simple way to not forget the a 'million' ideia.",
-};
+}; */
 
 export default function RootLayout({
   children,
@@ -19,17 +23,23 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <header>
-            <ModeToggle />
-          </header>
-          {children}
-        </ThemeProvider>
+        <QueryClientProvider client={queryClient}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <header>
+              <Container className="border-b border-zinc-300 dark:border-zinc-600 flex justify-between w-full">
+                <h1 className="text-2xl font-bold">Take a note</h1>
+
+                <ModeToggle />
+              </Container>
+            </header>
+            {children}
+          </ThemeProvider>
+        </QueryClientProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,30 @@
+"use client";
 import NoteCard from "@/components/NoteCard";
+import NoteProps from "@/types/note";
+import { useQuery } from "react-query";
+import axios from "axios";
+import NoteCardSkeleton from "@/components/NoteCard/NoteCardSkeleton";
 
 export default function Home() {
+  const { data: notes, isLoading } = useQuery<[NoteProps] | undefined>(
+    "userNotes",
+    async () => {
+      const { data } = await axios.get("http://localhost:4000/notes");
+      return data;
+    }
+  );
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <NoteCard />
+    <main className="min-h-screen p-24">
+      <div className="flex flex-wrap gap-2 items-start">
+        {notes
+          ? notes.map((elem, index) => {
+              console.log(elem);
+              return <NoteCard key={elem.title + index} {...elem} />;
+            })
+          : Array.from({ length: 8 }).map((_, index) => (
+              <NoteCardSkeleton key={`skeleton-${index}`} />
+            ))}
+      </div>
     </main>
   );
 }
