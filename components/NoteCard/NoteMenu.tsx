3648236c3f9d6295829549f526775d8029ea1781
Abraão Albuquerque <abraao.hs2@gmail.com>
2024-01-12 15:53:30 -0300

    :lipstick: adjusting style on notes menu

--- components/NoteCard/NoteMenu.tsx
+++ components/NoteCard/NoteMenu.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { toast } from "sonner";
-import { PinIcon, X } from "lucide-react";
+import { X } from "lucide-react";
 import useUserDataState from "@/stores/userDataStore";
 import NoteProps from "@/types/note";
 import { DrawingPinFilledIcon, DrawingPinIcon } from "@radix-ui/react-icons";
@@ -14,7 +14,7 @@ export default function NoteMenu({ note }: NoteMenuProps) {
   return (
     <div className="flex gap-1">
       <button
-        className="p-1 active:scale-8 dark:bg-zinc-700 rounded-md hover:text-red-400"
+        className="p-1 active:scale-8 dark:bg-zinc-700 hover:bg-zinc-200  dark:hover:bg-slate-400 rounded-md hover:text-red-400"
         aria-label="Delete note"
         type="button"
         onClick={() => {
@@ -36,7 +36,7 @@ export default function NoteMenu({ note }: NoteMenuProps) {
       </button>
       <button
         data-value={note.favorite ? "checked" : ""}
-        className="p-1 active:scale-8 dark:bg-zinc-700 dark:data-[value=checked]:bg-emerald-600 text-sm  rounded-md hover:text-slate-400"
+        className="p-1 active:scale-8 dark:bg-zinc-700 hover:bg-zinc-200 dark:hover:bg-slate-400 dark:data-[value=checked]:bg-emerald-600 data-[value=checked]:bg-emerald-400 text-sm  rounded-md hover:text-slate-500"
         aria-label="Toggle favorite"
         type="button"
         onClick={() => {
--- next.config.js
+++ next.config.js
@@ -1,8 +1,2 @@
-const million = require('million/compiler');
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = million.next(
-  nextConfig
-, { auto: { rsc: true } }
-)
+const nextConfig = {};
