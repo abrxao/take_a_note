4e62a131cf84b1270a992f61e678e7bd54ee4500
abrxao <abrxao@gmail.com>
2024-01-12 14:08:04 -0300

    seting metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,11 @@
 "use client";
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
-import ModeToggle from "@/components/ModeToggleButton";
 import { QueryClientProvider } from "react-query";
 import { queryClient } from "@/lib/queryClient";
-import Container from "@/components/Container";
-import NewNoteDrawer from "@/components/NewNoteDrawer";
 
 const inter = Inter({ subsets: ["latin"] });
-
-/* export const metadata: Metadata = {
-  title: "Take a note",
-  description: "Simple way to not forget the a 'million' ideia.",
-}; */
 
 export default function RootLayout({
   children,
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,16 @@
-"use client";
-import NoteCard from "@/components/NoteCard";
-import NoteProps from "@/types/note";
-import { useQuery } from "react-query";
-import axios from "axios";
-import NoteCardSkeleton from "@/components/NoteCard/NoteCardSkeleton";
 import ModeToggleButton from "@/components/ModeToggleButton";
-import NewNoteDrawer from "@/components/NewNoteDrawer";
 import Container from "@/components/Container";
+import UserNotesSection from "@/sections/UserNotesSection";
+import { Metadata } from "next";
+import NewNoteDrawer from "@/components/NewNoteDrawer";
+import { Toaster } from "@/components/ui/sonner";
+
+export const metadata: Metadata = {
+  title: "Take a note",
+  description: "Simple way to not forget the a 'million' ideia.",
+};
 
 export default function Home() {
-  const { data: notes, isLoading } = useQuery<[NoteProps] | undefined>(
-    "userNotes",
-    async () => {
-      const { data } = await axios.get("http://localhost:4000/notes");
-      return data;
-    }
-  );
   return (
     <>
       <header>
@@ -25,24 +20,15 @@ export default function Home() {
           <ModeToggleButton />
         </Container>
       </header>
-
       <Container>
-        <main>
-          <div className="flex flex-wrap gap-2 items-start">
-            {notes
-              ? notes.map((elem, index) => {
-                  console.log(elem);
-                  return <NoteCard key={elem.title + index} {...elem} />;
-                })
-              : Array.from({ length: 8 }).map((_, index) => (
-                  <NoteCardSkeleton key={`skeleton-${index}`} />
-                ))}
-          </div>
-          <div className="fixed lg:right-12 lg:bottom-12">
+        <main className="min-h-screen">
+          <div className="my-4">
             <NewNoteDrawer />
           </div>
+          <UserNotesSection />
         </main>
       </Container>
+      <Toaster />
     </>
   );
 }
