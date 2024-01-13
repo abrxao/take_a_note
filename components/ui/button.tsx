b2b59d3acaa0175a854e385e2da2c7c52f95b8b8
abrxao <abrxao@gmail.com>
2024-01-13 14:08:46 -0300

    some changes

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -24,7 +24,7 @@ const buttonVariants = cva(
       size: {
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
-        xsm: "h-7 rounded-md px-1 aspect-square",
+        xs: "h-7 rounded-md px-1 text-xs",
 
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
